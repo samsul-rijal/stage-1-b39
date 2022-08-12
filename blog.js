6ea751b17b5339da770a1b108880887670918638
Your Name <samsulr99@gmail.com>
2022-08-12 11:45:20 +0700

    day 5

--- blog.js
+++ blog.js
@@ -72,6 +72,30 @@ function addBlog(event){
     let title = document.getElementById("input-blog-title").value
     let content = document.getElementById("input-blog-content").value
     let image = document.getElementById("input-blog-image").files
+    
+    // let startDate = document.getElementById("start").value
+    // let endDate = document.getElementById("start").value
+
+    // console.log(new Date(startDate));
+    // console.log(endDate);
+
+    let twitter = document.getElementById("twitter").checked
+    let instagram = document.getElementById("instagram").checked
+
+    if(twitter){
+        twitter = document.getElementById("twitter").value
+    } else {
+        twitter = ''
+    }
+    if(instagram){
+        instagram = document.getElementById("instagram").value
+    } else {
+        instagram = ''
+    }
+
+    console.log(twitter);
+    console.log(instagram);
+
 
     // untuk membuat url gambar, agar tampil
     image = URL.createObjectURL(image[0])
@@ -81,7 +105,9 @@ function addBlog(event){
         content,
         image,
         author: "Samsul Rijal",
-        postAt: "11 Agustus 2022"
+        postAt: new Date(),
+        twitter,
+        instagram
     }
 
     dataBlog.push(blog)
@@ -116,13 +142,90 @@ function renderBlog(){
                     >
                 </h1>
                 <div class="detail-blog-content">
-                    ${dataBlog[index].postAt} | ${dataBlog[index].author}
+                    ${getFullTime(dataBlog[index].postAt)} | ${dataBlog[index].author}
                 </div>
                 <p>
                     ${dataBlog[index].content}
                 </p>
+
+                <i class="fa-brands fa-${dataBlog[index].twitter}"></i>
+                <i class="fa-brands fa-${dataBlog[index].instagram}"></i>
+
+                <div style="text-align: right; margin-top: 15px">
+                    <span style="font-size: 12px; color:grey">${getDistanceTime(dataBlog[index].postAt)}</span>
+                </div>
+
             </div>
         </div>
         `
     }
 }
+
+
+function getFullTime(time){
+
+    let month = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"]
+
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+
+    // console.log(date);
+    // console.log(month[monthIndex]);
+    // console.log(year);
+
+    // console.log(hours);
+    // console.log(minutes);
+
+    if(hours < 10){
+        hours = "0" + hours
+    }else if(minutes < 10){
+        minutes = "0" + minutes
+    }
+    
+    // 12 Agustus 2022 09.04
+    let fullTime = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`
+    // console.log(fullTime);
+    return fullTime
+}
+
+function getDistanceTime(time){
+
+    let timeNow = new Date()
+    let timePost = time
+
+    let distance = timeNow - timePost
+    console.log(distance);
+
+    let milisecond = 1000 // 1 detik 1000 milisecond
+    let secondInHours = 3600 // 1 jam sama dengan 3600 detik
+    let hoursInDay = 24 // 1 hari 24 jam
+
+    let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay))
+    let distanceHours = Math.floor(distance / (milisecond * 60 * 60))
+    let distanceMinutes = Math.floor(distance / (milisecond * 60))
+    let distanceSeconds = Math.floor(distance / milisecond)
+
+    if(distanceDay > 0){
+        return `${distanceDay} day ago`
+    } else if(distanceHours > 0){
+        return `${distanceHours} hours ago`
+    } else if(distanceMinutes > 0){
+        return `${distanceMinutes} minutes ago`
+    } else {
+        return `${distanceSeconds} seconds ago`
+    }
+}
+
+// function distanceDate(start, end){
+
+// }
+
+
+
+// setInterval(function(){
+//     renderBlog()
+// }, 3000)
